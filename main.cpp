83b8a0ad1deb0f13d0982f45e1f19f694dfd3847
Cong Chen <congchen5@gmail.com>
2014-04-17 22:27:40 -0700

    Have zooming working

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@ Window window = Window(400, 400);
 Scene scene = Scene();
 int window_id;
 GLuint obj_id;
+GLdouble dist = 5;
+GLdouble fov = 45;
 bool shading_toggle = 0;
 bool fill_toggle = 0;
 
@@ -38,33 +40,37 @@ void init()
 
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  glOrtho(-5, 5, -5, 5, -5.0, 5.0);
+  // gluLookAt(0,0,3.5,0,0,0,0,1,0);
+  // gluPerspective(fov, 1, -100, 100);
+  glOrtho(-dist, dist, -dist, dist, -100, 100);
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
+  // glFrustum(-5,5,-5,5,-5,5);
+  // gluLookAt(0,0,3.5,0,0,0,0,1,0);
 
   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); //Comment out if you want fill.
 
 
   GLfloat lpos[] = { 0.0f, 0.0f, -10.0f, 0 };
   glLightfv(GL_LIGHT0, GL_POSITION, lpos);
-  GLfloat lpos1[] = { 10.0f, 10.0f, 0.0f, 0 };
-  glLightfv(GL_LIGHT1, GL_POSITION, lpos1);
+  GLfloat ldiff[] = { 1.0f, 0.3f, 0.6f, 1.0 };
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, ldiff);
+  GLfloat lspec[] = { 1.0f, 1.0f, 1.0f, 1.00 };
+  glLightfv(GL_LIGHT0, GL_SPECULAR, lspec);
   
-  
+  GLfloat lpos1[] = { 0.0f, -10.0f, 0.0f, 0 };
+  glLightfv(GL_LIGHT1, GL_POSITION, lpos);
+  GLfloat ldiff1[] = { 1.0f, 0.3f, 0.6f, 1.0 };
+  glLightfv(GL_LIGHT1, GL_DIFFUSE, ldiff);
+  GLfloat lspec1[] = { 0.5f, 0.5f, 0.5f, 1.00 };
+  glLightfv(GL_LIGHT1, GL_SPECULAR, lspec);
+
   glEnable(GL_DEPTH_TEST);		// enable hidden surface removal
   glEnable(GL_LIGHTING);		// enable lighting
   glEnable(GL_LIGHT0);		// enable
-  // glEnable(GL_LIGHT1);
-	
-  // glShadeModel(GL_FLAT);		// flat shading
-  // glShadeModel(GL_SMOOTH);		// smooth shading 
-
-  // glEnable(GL_FLAT);
-  // glShadeModel(GL_FLAT);		// flat shading
-  // glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-  // glShadeModel(GL_SMOOTH);		// smooth shading */
-
+  glEnable(GL_LIGHT1);
+ 
   // glRotatef(270.0f, 1, 0, 0);
   //glRotatef(180.0f, 1, 0, 0);
 }
@@ -73,8 +79,8 @@ void createObj() {
   obj_id = glGenLists(1);
   glNewList(obj_id, GL_COMPILE);
   for (int i = 0; i < scene.patches.size(); i++) {
-    window.drawAdaptive(*scene.patches.at(i),0.01f);
-    // window.drawUniform(*scene.patches.at(i),1.0f/10.0f);
+    // window.drawAdaptive(*scene.patches.at(i),0.1f);
+    window.drawUniform(*scene.patches.at(i),1.0f/10.0f);
   }
   glEndList();
 }
@@ -94,15 +100,25 @@ void display() {
 
 void keyPressed (unsigned char key, int x, int y) {
   float rotation_degree = 10.0f;
-  if (key == 43) { // + is pressed
-    std::cout << "+ pressed" << std::endl;
-    glTranslatef(0, 0, 2);
+  if ((key == 43) || (key == 61)) { // + is pressed
+    dist -= 0.5;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-dist, dist, -dist, dist, -100, 100);
+    glMatrixMode(GL_MODELVIEW);
+    glutPostRedisplay();
   }
-  if (key == 45) { // - is pressed
-
+  else if ((key == 45) || (key == 95)) { // - is pressed
+    dist += 0.5;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-dist, dist, -dist, dist, -100, 100);
+    glMatrixMode(GL_MODELVIEW);
+    glutPostRedisplay();
   }
-  if (key == 115) { // s is pressed
+  else if (key == 115) { // s is pressed
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    fill_toggle = 0;
     if (shading_toggle) {
       glShadeModel(GL_SMOOTH);
     }
@@ -113,7 +129,7 @@ void keyPressed (unsigned char key, int x, int y) {
     shading_toggle = !shading_toggle;
     glutPostRedisplay();
   }
-  if (key == 119) { // w is pressed
+  else if (key == 119) { // w is pressed
     if (fill_toggle) {
       glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     }
@@ -124,7 +140,7 @@ void keyPressed (unsigned char key, int x, int y) {
     fill_toggle = !fill_toggle;
     glutPostRedisplay();
   }
-  if (key == 32) { // If the space key is pressed
+  else if (key == 32) { // If the space key is pressed
     glutDestroyWindow ( window_id );
     exit(0);
   }
@@ -141,7 +157,7 @@ void keyPressed2 (int key, int x, int y) {
       glRotatef(rotation_degree, -1, 0, 0);
     }
   }
-  if (key == GLUT_KEY_DOWN) {
+  else if (key == GLUT_KEY_DOWN) {
     if (glutGetModifiers() & GLUT_ACTIVE_SHIFT) {
       glTranslatef(0,-0.5,0);
     }
@@ -149,7 +165,7 @@ void keyPressed2 (int key, int x, int y) {
       glRotatef(rotation_degree, 1, 0, 0);
     }
   }
-  if (key == GLUT_KEY_RIGHT) {
+  else if (key == GLUT_KEY_RIGHT) {
     if (glutGetModifiers() & GLUT_ACTIVE_SHIFT) {
       glTranslatef(0.5,0,0);
     }
@@ -157,7 +173,7 @@ void keyPressed2 (int key, int x, int y) {
       glRotatef(rotation_degree, 0, 0, 1);
     }
   }
-  if (key == GLUT_KEY_LEFT) {
+  else if (key == GLUT_KEY_LEFT) {
     if (glutGetModifiers() & GLUT_ACTIVE_SHIFT) {
       glTranslatef(-0.5,0,0);
     }
